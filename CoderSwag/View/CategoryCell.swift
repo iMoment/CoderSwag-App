f4faf2e9147c7e7e6a7fe5447bbfa8ec64d46efb
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-09 21:14:48 +0900

    Added assets.

---
+++ CoderSwag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  CoderSwag
+//
+//  Created by Stanley Pan on 09/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
