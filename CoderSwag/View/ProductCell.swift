54d1b9512854adb5a618a1f7b942ad42e3f9fd3a
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-10 03:57:42 +0900

    Add CollectionViewCell

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Stanley Pan on 10/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productTitleLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    
+}
